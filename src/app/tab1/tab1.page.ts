9749caa871d430bd98f179b3ca1bed6d3f5d590a
Winter Gerlach <winter@talixa.com>
2022-11-27 15:15:35 -0700

    radio buttons working on lessons

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit  } from '@angular/core';
-import {lessons} from '../data/lessons.js';
+import { lessonData } from '../data/lessons.js';
 
 @Component({
   selector: 'app-tab1',
@@ -7,20 +7,33 @@ import {lessons} from '../data/lessons.js';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  lessons = lessons.lessons;
-  state = 'select'
-  id = 0
-  answer=""
+
+  lessons = lessonData;
+  state = 'select';
+  id = 0;
+  answer='';
+  selectedAnswer = 0;
+
   constructor()
   {}
 
-  selected(id: number):void {
-    this.state = "lesson";
+  selected(id: number): void {
+    this.state = 'lesson';
     this.id = id;
     //may need more magic code
   }
 
   back(): void{
-    this.state = "select";
+    this.state = 'select';
+  }
+
+  check(): void {
+    console.log(typeof(this.selectedAnswer));
+    console.log(typeof(this.lessons[this.id].comprehension.correct));
+    if (this.selectedAnswer === this.lessons[this.id].comprehension.correct.toString()) {
+      this.id += 1;
+    } else {
+      alert ('you suck!');
+    }
   }
 }
